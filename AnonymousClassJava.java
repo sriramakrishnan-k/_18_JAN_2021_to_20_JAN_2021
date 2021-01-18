f3f733417a74c85e585a079775f2aafd1f492205
sriramakrishnan-k <77046069+sriramakrishnan-k@users.noreply.github.com>
2021-01-18 19:14:58 +0530

    files added

---
+++ AnonymousClassJava.java
@@ -0,0 +1,110 @@
+abstract 
+    class Anonymous{
+
+        abstract public
+            void show();
+
+        public 
+            Anonymous(){
+                System.out.println("construct of abstract class Anonymous");
+    }
+
+}
+
+interface AnonymousInterface{
+
+        abstract public void display();
+
+}
+
+public 
+    class AnonymousClassJava {
+
+    public static 
+        void main(String args[]){
+
+        // anonymous class extends Anonymous
+            Anonymous objectAnonymous = new Anonymous(){
+
+                public 
+                    int anonymousInstanceVariable = 2000;
+                
+                //instance initializer
+                {
+                    System.out.println("instance initializer in Anonymous_class");
+
+                    System.out.println("anonymousInstanceVariable = " + anonymousInstanceVariable);
+                    anonymousInstanceVariable = 5000;
+                    System.out.println("anonymousInstanceVariable = " + anonymousInstanceVariable);
+                }
+
+                @Override public 
+                    void show(){
+                        System.out.printf("show member in Anonymous Class");                                       
+                }
+
+            };
+
+            objectAnonymous.show();
+            objectAnonymous.show();
+            
+
+            AnonymousInterface objectAnonymousInterface = new AnonymousInterface(){
+
+                @Override public
+                    void display(){
+                        System.out.println("display member in Anonymous Interface");
+                }
+
+            };
+
+            objectAnonymousInterface.display();
+
+
+            // all the classes in java extends Object class
+            // hence to create a anonymous class it is better to use Object class name
+            new Object(){
+                
+                static final 
+                    int staticVariable = 200; // final value are allowed (if static it is a static field)
+                
+                int instanceVariable = 101;
+
+                // instance initializer block
+                {
+                    System.out.println("before --> instanceVariable = "+ this.instanceVariable);
+                    System.out.println("instance initializer block of anonymous class(Object class extended)");
+                    this.instanceVariable = 10001;
+                    System.out.println("After --> instanceVariable = "+ this.instanceVariable);
+                }
+
+                /* 
+                static initializer block is not allowed in anonymous class 
+                construct is not allow
+                but instance initializer block is allowed
+                */
+
+                public 
+                    void function(){
+                        System.out.println(" function in anonymous ");
+                        System.out.println("static variable  = "  + staticVariable);
+                        System.out.println("instance variable = " + instanceVariable);
+                    }
+
+            }.function();
+
+            /* if we try to store the object of anonymous class in Object type variables
+                like
+                    Object obj = new Object(){
+                                                static fields,
+                                                static methods,
+                                                instance attributes,
+                                                instance methods,
+                                                overrided methods
+                                             };
+
+                    * we can access the overrided methods using obj *
+                   ( *** ) remaining members are not access through obj.
+            */
+    }
+}
---
+++ SingletonClass.java
@@ -0,0 +1,44 @@
+class Singleton{
+
+    private 
+        String instanceVariable;
+
+    private static 
+        Singleton singleObject = null;
+
+    private Singleton(String instanceVariable){
+        this.instanceVariable = instanceVariable;
+    }
+
+    static Singleton getInstance(String instanceVariable){
+
+        if (singleObject == null){
+            singleObject = new Singleton(instanceVariable);
+        }
+        return singleObject;
+    }
+
+    public String getInstanceVariable(){
+        return this.instanceVariable;
+    }
+
+    public void setInstanceVariable(String instanceVariable){
+        this.instanceVariable = instanceVariable;
+    }
+    
+}
+
+public class SingletonClass {
+    public static void main(String args[]){
+
+        Singleton obj1 = Singleton.getInstance("instanceVariable - obj1");
+        Singleton obj2 = Singleton.getInstance("instanceVariable - obj2");
+
+        System.out.println(obj1.getInstanceVariable());
+        obj2.setInstanceVariable("instanceVariable - obj2");
+        System.out.println(obj2.getInstanceVariable());
+        
+        System.out.println(obj1.hashCode() == obj2.hashCode());
+
+    }
+}
